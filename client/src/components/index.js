16eaf9f36cdf175a866d16670d762c98bc660502
Stephan Collins <Stephan123lel@gmail.com>
2024-08-27 10:50:45 -0500

    React jsx

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Layout} from './Layout.jsx'
+export {default as Public} from './Public.jsx'
+export {default as DashLayout} from './DashLayout.jsx'
+export {default as DashHeader} from './DashHeader.jsx'
+export {default as DashFooter} from './DashFooter.jsx'
---
+++ client/src/features/users/UsersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <h1>
+      Users List
+    </h1>
+  )
+}
+
+export default UsersList
