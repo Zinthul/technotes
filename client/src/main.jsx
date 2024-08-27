cdf42ec8e2fa9c3d2660990bd108d63ebb14c7d8
Stephan Collins <Stephan123lel@gmail.com>
2024-08-27 12:39:49 -0500

    Redux & RTK Query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
