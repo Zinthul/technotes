28e62ed1dc58b0d7a982b533d81e257d87a3bc70
Stephan Collins <Stephan123lel@gmail.com>
2024-08-29 13:42:09 -0500

    using useGetQuery in children components

--- client/src/features/notes/NewNote.jsx
+++ client/src/features/notes/NewNote.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import NewNoteForm from './NewNoteForm' 
-import { selectAllUsers } from '../users/usersApiSlice'
+import React from "react";
+import NewNoteForm from "./NewNoteForm";
+import { useGetUsersQuery } from "../users/usersApiSlice";
+import PulseLoader from "react-spinners/PulseLoader";
 
 const NewNote = () => {
-    const users = useSelector(selectAllUsers)
+  const { users } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      users: data?.ids.map((id) => data?.entities[id]),
+    }),
+  });
 
-    if (!users?.length) return <p>Not Currently Available</p>
+  if (!users?.length) return <PulseLoader color={"#FFF"} />;
 
-    const content = <NewNoteForm users={users} />
-    // const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+  const content = <NewNoteForm users={users} />;
 
-  return content
-}
+  return content;
+};
 
-export default NewNote
+export default NewNote;
--- client/src/features/users/User.jsx
+++ client/src/features/users/User.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { useGetUsersQuery } from "./usersApiSlice";
+import { memo } from 'react'
 
-import { useSelector } from "react-redux";
-import { selectUserById } from "./usersApiSlice";
 
 const User = ({ userId }) => {
-  const user = useSelector((state) => selectUserById(state, userId));
+
+    const user = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            user: data?.entities[userId]
+        })
+    })
 
   const navigate = useNavigate();
 
@@ -32,4 +37,6 @@ const User = ({ userId }) => {
   } else return null;
 };
 
-export default User;
+const memoizedUser= memo(User)
+
+export default memoizedUser;
